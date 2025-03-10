3e9383bb343df67a820733e5f72b7ecb1cb0c18e
caio prezotto <152824103+caioprezottoo@users.noreply.github.com>
2025-03-10 17:50:52 -0300

    first commit

---
+++ PRIMEIRO_PROJETO.py
@@ -0,0 +1,40 @@
+'''
+1.
+import math
+raio = float(input("Digite o raio do círculo: "))
+area = math.pi * raio**2
+print("A área do círculo é:", area)
+'''
+
+'''
+2.
+celsius = float(input("Temperatura em Celsius: "))
+fahrenheit = (celsius*9/5)+32
+print("A temperatura em Fahrenheit: ", fahrenheit)
+'''
+
+'''
+3.
+n1 = int(input("Número 1: "))
+n2 = int(input("Número 2: "))
+n3 = int(input("Número 3: "))
+
+media = (n1+n2+n3)//3
+print("Média: ", media)
+'''
+
+'''
+4.
+horas = float(input("Horas trabalhadas: "))
+vPorHora = float(input("Valor por hora: "))
+
+salario = vPorHora*horas
+print("Salário: ", salario)
+'''
+'''
+5.
+m = float(input("Metros: "))
+cm = m*100
+print(m, "metro(s) = ", cm, "centimetro(s)")
+'''
+
